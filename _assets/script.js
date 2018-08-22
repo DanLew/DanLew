6800d10c1c8dc7b852303ff5cc9ee3ae942d957c
DanLew <danlewis225@gmail.com>
2018-08-22 16:01:09 +0100

    Created First Block

---
+++ _assets/script.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($){
+
+  setTimeout(function(){
+
+    startPage();
+
+  }, 1000);
+
+  function startPage() {
+
+    $('.circle.start').removeClass('start');
+    $('.title.start').removeClass('start');
+    $('.sub-title.start').removeClass('start');
+
+  }
+
+});
